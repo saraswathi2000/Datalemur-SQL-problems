bf137eba7166b25c6dae64b12779a34dd374fd0b
Saraswathi Pandit <72403496+saraswathi2000@users.noreply.github.com>
2022-12-23 20:41:17 +0530

    Data Science Skills

---
+++ SQL - EASY/Data Science Skills.sql
@@ -0,0 +1,16 @@
+/*
+Difficulty Level : Easy
+Challenge : Data Science Skills ( LinkedIn SQL Interview Question)
+
+Given a table of candidates and their skills, you're tasked with finding the candidates best suited for an open Data Science job. 
+You want to find candidates who are proficient in Python, Tableau, and PostgreSQL.
+
+Write a query to list the candidates who possess all of the required skills for the job. Sort the the output by candidate ID in ascending order.*/
+
+
+SELECT candidate_id 
+FROM candidates 
+WHERE skill in ('Python','Tableau','PostgreSQL')
+GROUP BY candidate_id 
+HAVING count(skill)=3 
+ORDER BY candidate_id;
