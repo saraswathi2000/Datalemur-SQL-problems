b97679cb7a38e3029a42c6c9639dba95d9031975
Saraswathi Pandit <72403496+saraswathi2000@users.noreply.github.com>
2022-12-29 14:12:18 +0530

    Update Page With No Likes.sql

--- SQL - EASY/Page With No Likes.sql
+++ SQL - EASY/Page With No Likes.sql
@@ -1,6 +1,6 @@
 /* 
 Difficulty Level : Easy
-Challenge : Page with no likes ( facebook SQL interview Question)
+Challenge : Page with no likes (facebook SQL interview Question)
 
 Assume you are given the tables below about Facebook pages and page likes. 
 Write a query to return the page IDs of all the Facebook pages that don't have any likes. The output should be in ascending order.*/
