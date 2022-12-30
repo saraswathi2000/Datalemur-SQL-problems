f36ba6b21b6acdfab22c759123c66973bc1aa72f
Saraswathi Pandit <72403496+saraswathi2000@users.noreply.github.com>
2022-12-30 14:53:56 +0530

    Create Laptop vs mobile viewership.sql

---
+++ SQL - EASY/Laptop vs mobile viewership.sql
@@ -0,0 +1,12 @@
+/*
+Difficulty Level : Easy
+Challenge : [New York Times SQL Interview Question]
+
+Assume that you are given the table below containing information on viewership by device type (where the three types are laptop, tablet, and phone). 
+Define “mobile” as the sum of tablet and phone viewership numbers. Write a query to compare the viewership on laptops versus mobile devices.
+
+Output the total viewership for laptop and mobile devices in the format of "laptop_views" and "mobile_views". */
+
+SELECT SUM(CASE WHEN device_type = 'laptop' THEN 1 ELSE 0 END) AS laptop_views,
+SUM(CASE WHEN device_type IN ('phone','tablet') THEN 1 ELSE 0 END) AS mobile_views
+FROM viewership;
